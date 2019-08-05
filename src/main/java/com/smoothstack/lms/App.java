cfb9393cb1ce53c128748ecd486dbafd308a19d5
Skyler Daché <skyberskyman@gmail.com>
2019-08-04 23:33:50 -0400

    Other Initial commits

--- src/main/java/com/smoothstack/lms/App.java
+++ src/main/java/com/smoothstack/lms/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for (int i=0; i< args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
