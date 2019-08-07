19f364570074568df2477bead0d4ceb3b76cafd8
Skyler Daché <skyberskyman@gmail.com>
2019-08-07 06:19:55 -0400

    Developed menus

--- src/main/java/com/smoothstack/lms/Menus.java
+++ src/main/java/com/smoothstack/lms/Menus.java
@@ -3,9 +3,26 @@ package com.smoothstack.lms;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.smoothstack.lms.services.AuthorService;
+import com.smoothstack.lms.services.BookService;
+import com.smoothstack.lms.services.PublisherService;
+
 public class Menus {
     static void mainMenu() {
-        int choice = -1;
+        mainMenu(-1);
+    }
+    static void mainMenu(int n) {
+        mainMenu(n, -1);
+    }
+    static void mainMenu(int mainChoice, int subChoice) {
+        /**
+         * the choice integer represents the menu choices available to a user
+         * choice =  0 means exiting the menu
+         * choice = -1 means the user has not made a choice and must be prompted
+         * choice > 1 means the user does not need to be prompted, and will 
+         *            immediately got to the next menu
+         */
+        int choice = mainChoice;
         System.out.println("Welcome to the Library Management System");
         String[] menu = new String[] {
             "Enter a number to select an operation:/n",
@@ -17,6 +34,69 @@ public class Menus {
             "0.) Exit"
         };
         do {
+            if (choice < 0) {
+                for (String s : menu) {System.out.println(s);}
+                try (Scanner in = new Scanner(System.in)) {
+                    choice = in.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter an integer from 0 to 5");
+                    continue;
+                }
+            }
+            switch (choice) {
+            case 1: list(subChoice); break;
+            case 2: search(subChoice); break;
+            case 3: add(subChoice); break;
+            case 4: update(subChoice); break;
+            case 5: delete(subChoice); break;
+            case 0: break;
+            default:
+                System.out.println("Please enter an integer from 0 to 5");
+            }
+            if (choice != 0) choice = -1;
+        } while (choice != 0);
+        System.out.println("Goodbye");
+    }
+    static void list() {
+        mainMenu(1, -1);
+    }
+    static void list(int choice) {
+        String[] menu = new String[] {
+            "Select one of the following to list (enter a number):",
+            "1.) List Authors",
+            "2.) List Books",
+            "3.) List Publishers",
+            "0.) Go back to Main Menu"
+        };
+        do {
+            for (String s : menu) {System.out.println(s);}
+            try (Scanner in = new Scanner(System.in)) {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer from 0 to 3");
+                continue;
+            }
+            switch (choice) {
+            case 0: break;
+            case 1: AuthorService.list(); break;
+            case 2: BookService.list(); break;
+            case 3: PublisherService.list(); break;
+            default: System.out.println("Selection out of range\n");
+            }
+        } while (choice != 0);
+    }
+    static void search() {
+        mainMenu(2, -1);
+    }
+    static void search(int choice) {
+        String[] menu = new String[] {
+            "Select one of the following to search (enter a number):",
+            "1.) Search Authors",
+            "2.) Search Books",
+            "3.) Search Publishers",
+            "0.) Back to main menu"
+        };
+        do {
             for (String s : menu) {System.out.println(s);}
             try (Scanner in = new Scanner(System.in)) {
                 choice = in.nextInt();
@@ -25,21 +105,95 @@ public class Menus {
                 continue;
             }
             switch (choice) {
-            case 1: list(); break;
-            case 2: search(); break;
-            case 3: add(); break;
-            case 4: update(); break;
-            case 5: delete(); break;
             case 0: break;
-            default:
-                System.out.println("Please enter an integer from 0 to 5");
+            case 1: AuthorService.search(); break;
+            case 2: BookService.search(); break;
+            case 3: PublisherService.search(); break;
+            default: System.out.println("Selection out of range");
             }
         } while (choice != 0);
-        System.out.println("Goodbye");
     }
-    static void list() {}
-    static void add() {}
-    static void search() {}
-    static void update() {}
-    static void delete() {}
+    static void add() {
+        mainMenu(3, -1);
+    }
+    static void add(int choice) {
+        String[] menu = new String[] {
+            "Select one of the following to create (enter a number):",
+            "1.) Create Author",
+            "2.) Create Book",
+            "3.) Create Publisher",
+            "0.) Back to main menu"
+        };
+        do {
+            for (String s : menu) {System.out.println(s);}
+            try (Scanner in = new Scanner(System.in)) {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer from 0 to 3");
+                continue;
+            }
+            switch (choice) {
+            case 0: break;
+            case 1: AuthorService.add(); break;
+            case 2: BookService.add(); break;
+            case 3: PublisherService.add(); break;
+            default: System.out.println("Selection out of range.");
+            }
+        } while (choice != 0);
+    }
+    static void update() {
+        mainMenu(4, -1);
+    }
+    static void update(int choice) {
+        String[] menu = new String[] {
+            "Select one of the following to update (enter a number):",
+            "1.) Update Author",
+            "2.) Update Book",
+            "3.) Update Publisher",
+            "0.) Back to main menu"
+        };
+        do {
+            for (String s : menu) {System.out.println(s);}
+            try (Scanner in = new Scanner(System.in)) {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer from 0 to 5");
+                continue;
+            }
+            switch (choice) {
+                case 0: break;
+                case 1: AuthorService.update();
+                case 2: BookService.update();
+                case 3: PublisherService.update();
+            }
+        } while (choice != 0);
+    }
+    static void delete() {
+        mainMenu(5, -1);
+    }
+    static void delete(int choice) {
+        String[] menu = new String[] {
+            "Select one of the following to delete (enter a number):",
+            "1.) Delete Author",
+            "2.) Delete Book",
+            "3.) Delete Publisher",
+            "0.) Back to main menu"
+        };
+        do {
+            for (String s : menu) {System.out.println(s);}
+            try (Scanner in = new Scanner(System.in)) {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer from 0 to 5");
+                continue;
+            }
+            switch (choice) {
+            case 0: break;
+            case 1: AuthorService.delete(); break;
+            case 2: BookService.delete(); break;
+            case 3: PublisherService.delete(); break;
+            default: System.out.println("Selection out of range");
+            }
+        } while (choice != 0);
+    }
 }
